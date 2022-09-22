d5928096c5f3e935a0126794917219233963822b
Marcel Neumann <mrclneumann@users.noreply.github.com>
2022-09-22 18:33:59 +0200

    Add test cases

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -13,3 +13,23 @@ from game_of_life import update
 )
 def test_update(grid, expected):
     assert_array_equal(update(np.array(grid)), expected)
+
+
+def test_blinker():
+    assert_array_equal(
+        update(np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]])),
+        [[0, 0, 0], [1, 1, 1], [0, 0, 0]],
+    )
+
+
+@pytest.mark.parametrize(
+    "grid",
+    [
+        [[1, 1], [1, 1]],
+        [[0, 1, 0], [1, 0, 1], [0, 1, 0]],
+        [[0, 1, 1, 0], [1, 0, 0, 1], [1, 0, 0, 1], [0, 1, 1, 0]],
+    ],
+    ids=["block", "tub", "pond"],
+)
+def test_static_objects(grid):
+    assert_array_equal(update(np.array(grid)), grid)
